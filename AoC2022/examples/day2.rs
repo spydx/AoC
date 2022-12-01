a7f3a96a0bb9e6dfb3f45558c66cf3aae813c834
Kenneth Fossen <fossen.kenneth@gmail.com>
2022-12-01 20:17:52 +0100

    day two prep and template

---
+++ AoC2022/examples/day2.rs
@@ -0,0 +1,23 @@
+use aoc_2022::{read_lines};
+
+const DAY:&str = "day2.txt";
+
+fn main() {
+
+    part_one();
+
+    part_two();
+}
+
+fn part_one() {
+    println!("PART I");
+    let content = read_lines(DAY).unwrap();
+    todo!()
+}
+
+fn part_two() {
+    println!("PART II");
+    let content = read_lines(DAY).unwrap();
+
+    todo!()
+}
---
+++ AoC2022/examples/template.rs
@@ -0,0 +1,22 @@
+use aoc_2022::{read_lines};
+
+const DAY:&str = ".txt";
+
+fn main() {
+
+    part_one();
+
+    part_two();
+}
+
+fn part_one() {
+    println!("PART I");
+    let content = read_lines(DAY).unwrap();
+    todo!()
+}
+
+fn part_two() {
+    println!("PART II");
+    let content = read_lines(DAY).unwrap();
+    todo!()
+}
