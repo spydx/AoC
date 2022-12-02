8cf4ea4d6733bb468afbea4f84b443e871a54ee8
Kenneth Fossen <fossen.kenneth@gmail.com>
2022-12-02 15:05:14 +0100

    day2

--- AoC2021/Rust/examples/template.rs
+++ AoC2021/Rust/examples/template.rs
@@ -1,4 +1,4 @@
-use aoc_2022::read_lines;
+use aoc_2021::read_lines;
 use std::time::Instant;
 
 const DAY: &str = ".txt";
@@ -15,12 +15,12 @@ fn main() {
 
 fn part_one() {
     println!("PART I");
-    let content = read_lines(DAY).unwrap();
+    let _content = read_lines(DAY).unwrap();
     todo!()
 }
 
 fn part_two() {
     println!("PART II");
-    let content = read_lines(DAY).unwrap();
+    let _content = read_lines(DAY).unwrap();
     todo!()
 }
--- AoC2022/Rust/examples/day2.rs
+++ AoC2022/Rust/examples/day2.rs
@@ -8,15 +8,72 @@ fn main() {
     part_two();
 }
 
+
 fn part_one() {
     println!("PART I");
     let content = read_lines(DAY).unwrap();
-    todo!()
+    // A - Rock - X = 1
+    // B - Paper - Y = 2
+    // C - Sciccor - Z = 3
+    // Loss = 0
+    // Draw = 3
+    // Win = 6
+    let mut score = vec![0];
+    for line in content {
+        if let Ok(current_line) = line {
+            let oponent = current_line.chars().nth(0).unwrap();
+            let me = current_line.chars().last().unwrap();
+            score.push(score_game(oponent, me));
+        }
+    }
+    let res: i32 = score.iter().sum();
+    println!("Sum: {}", res);
+}
+
+fn score_game(p1: char, p2: char) -> i32 {
+    
+    let winner = match (p1, p2) { 
+        ('A','X') => 1+3,
+        ('B','X') => 1+0,
+        ('C','X') => 1+6,
+        ('A','Y') => 2+6,
+        ('B','Y') => 2+3,
+        ('C','Y') => 2+0,
+        ('A','Z') => 3+0,
+        ('B','Z') => 3+6,
+        ('C','Z') => 3+3,
+        _ => panic!("Something went wrong")
+    };
+    winner
+}
+
+fn get_move(p1: char, plan: char) -> char {
+    match (p1, plan) {
+        ('A','X') => 'Z',
+        ('A','Y') => 'X',
+        ('A','Z') => 'Y',
+        ('B','X') => 'X',
+        ('B','Y') => 'Y',
+        ('B','Z') => 'Z',
+        ('C','X') => 'Y',
+        ('C','Y') => 'Z',
+        ('C','Z') => 'X',
+        _ => panic!("Something went wrong")
+    }
 }
 
 fn part_two() {
     println!("PART II");
     let content = read_lines(DAY).unwrap();
-
-    todo!()
+    let mut score = vec![0];
+    for line in content {
+        if let Ok(current_line) = line {
+            let oponent = current_line.chars().nth(0).unwrap();
+            let plan = current_line.chars().last().unwrap();
+            let me = get_move(oponent, plan);
+            score.push(score_game(oponent, me));
+        }
+    }
+    let res: i32 = score.iter().sum();
+    println!("Sum: {}", res);
 }
