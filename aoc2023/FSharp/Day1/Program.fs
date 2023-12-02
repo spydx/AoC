51eabb468a01fe5929f0b53c1fd7f6e55e250204
Kenneth Fossen <fossen.kenneth@gmail.com>
2023-12-02 12:34:20 +0100

    Reorganising for FSsharp 2023

---
+++ aoc2023/FSharp/Day1/Program.fs
@@ -0,0 +1,9 @@
+﻿open System.IO
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let lines = File.ReadAllLines(@"day1.txt")
+let input = Seq.toList lines
+
+printfn $"%A{input}"
---
+++ aoc2023/FSharp/README.md
@@ -0,0 +1,7 @@
+# FSharp
+
+New project:
+```sh
+dotnet new console -n Day1 -lang "F#"
+dotnet sln add <project>
+```
