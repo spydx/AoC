2266b5fb1fa2c9a1ee746f0d256771bb7f3fe494
Kenneth Fossen <fossen.kenneth@gmail.com>
2024-12-01 11:39:55 +0100

    2024: d1

--- aoc2024/Rust/examples/day1.rs
+++ aoc2024/Rust/examples/day1.rs
@@ -1,25 +1,69 @@
 fn main() {
-    let file = include_str!("dayX.txt");
+    let file = include_str!("day1.txt");
     let part1_res = part1(file);
     println!("Part1: {}", part1_res);
     let part2_res = part2(file);
     println!("Part2: {}", part2_res);
 }
 
-fn part1(_file: &str) -> usize {
-    0
+fn part1(file: &str) -> usize {
+    let mut lhs = vec![];
+    let mut rhs = vec![];
+    for line in file.lines() {
+        let (left, right) = part1_linesolve(&line);
+        lhs.push(left);
+        rhs.push(right);
+    }
+
+    lhs.sort();
+    rhs.sort();
+
+    let mut res: Vec<usize> = vec![];
+    if lhs.len() != rhs.len() {
+        panic!("Not the same length");
+    }
+
+    for i in 0..lhs.len() {
+        let l = lhs[i];
+        let r = rhs[i];
+        let distance = l - r;
+        res.push(distance.abs() as usize);
+    }
+
+    res.iter()
+        .sum()
 }
 
-fn part1_linesolve(_line: &str) -> usize {
-    0
+fn part1_linesolve(_line: &str) -> (i32, i32) {
+    let parts = _line.split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+    (parts[0], parts[1])
 }
 
-fn part2(_file: &str) -> usize {
-    0
-}
+fn part2(file: &str) -> i32 {
+    let mut lhs = vec![];
+    let mut rhs = vec![];
+    for line in file.lines() {
+        let (left, right) = part1_linesolve(&line);
+        lhs.push(left);
+        rhs.push(right);
+    }
 
-fn part2_linesolve(_line: &str) -> usize {
-    0
+    let mut res: Vec<i32> = vec![];
+    for i in 0..lhs.len() {
+        let l = lhs[i];
+        let matches: Vec<&i32>= rhs.iter()
+            .filter(|&r| r == &l)
+            .collect();
+
+        let similarityscore = l * matches.len() as i32;
+        res.push(similarityscore)
+
+    }
+
+    res.iter()
+        .sum()
 }
 
 #[cfg(test)]
@@ -27,7 +71,16 @@ mod test {
     use super::*;
 
     #[test]
-    fn part1_input() {}
+    fn part1_input() {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+       let res = part1(input);
+        assert_eq!(res, 11);
+    }
 
     #[test]
     fn part1_lineinput() {}
@@ -36,7 +89,16 @@ mod test {
     fn part1_lineinput_falsy() {}
 
     #[test]
-    fn part2_input() {}
+    fn part2_input() {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        let res = part2(input);
+        assert_eq!(res, 31);
+    }
 
     #[test]
     fn part2_lineinput() {}
